6767e2d3a790a7135b88f5181e8aed7d2c4f3bce
Luixc <44784094+Luixc@users.noreply.github.com>
2021-02-10 11:17:19 -0500

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NavigationService } from './services/navigation.service';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'compilador';
+  constructor(public service: NavigationService){
+
+  }
+  get _service(){
+    return this.service;
+  }
+}
